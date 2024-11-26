fe2e68db0b566ed3ac361fd6be29e3b2dcc946af
Roshan Shah <118169938+Roshan-Shah-7@users.noreply.github.com>
2024-11-26 12:35:42 +0545

    Add files via upload

---
+++ evcolor.sql
@@ -0,0 +1,259 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 26, 2024 at 05:37 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `evcolor`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_actionscheduler_actions`
+--
+-- Error reading structure for table evcolor.wp_actionscheduler_actions: #1932 - Table 'evcolor.wp_actionscheduler_actions' doesn't exist in engine
+-- Error reading data for table evcolor.wp_actionscheduler_actions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_actionscheduler_actions`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_actionscheduler_claims`
+--
+-- Error reading structure for table evcolor.wp_actionscheduler_claims: #1932 - Table 'evcolor.wp_actionscheduler_claims' doesn't exist in engine
+-- Error reading data for table evcolor.wp_actionscheduler_claims: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_actionscheduler_claims`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_actionscheduler_groups`
+--
+-- Error reading structure for table evcolor.wp_actionscheduler_groups: #1932 - Table 'evcolor.wp_actionscheduler_groups' doesn't exist in engine
+-- Error reading data for table evcolor.wp_actionscheduler_groups: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_actionscheduler_groups`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_actionscheduler_logs`
+--
+-- Error reading structure for table evcolor.wp_actionscheduler_logs: #1932 - Table 'evcolor.wp_actionscheduler_logs' doesn't exist in engine
+-- Error reading data for table evcolor.wp_actionscheduler_logs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_actionscheduler_logs`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_commentmeta`
+--
+-- Error reading structure for table evcolor.wp_commentmeta: #1932 - Table 'evcolor.wp_commentmeta' doesn't exist in engine
+-- Error reading data for table evcolor.wp_commentmeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_commentmeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_comments`
+--
+-- Error reading structure for table evcolor.wp_comments: #1932 - Table 'evcolor.wp_comments' doesn't exist in engine
+-- Error reading data for table evcolor.wp_comments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_comments`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_e_events`
+--
+-- Error reading structure for table evcolor.wp_e_events: #1932 - Table 'evcolor.wp_e_events' doesn't exist in engine
+-- Error reading data for table evcolor.wp_e_events: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_e_events`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_links`
+--
+-- Error reading structure for table evcolor.wp_links: #1932 - Table 'evcolor.wp_links' doesn't exist in engine
+-- Error reading data for table evcolor.wp_links: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_links`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_options`
+--
+-- Error reading structure for table evcolor.wp_options: #1932 - Table 'evcolor.wp_options' doesn't exist in engine
+-- Error reading data for table evcolor.wp_options: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_options`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_postmeta`
+--
+-- Error reading structure for table evcolor.wp_postmeta: #1932 - Table 'evcolor.wp_postmeta' doesn't exist in engine
+-- Error reading data for table evcolor.wp_postmeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_postmeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_posts`
+--
+-- Error reading structure for table evcolor.wp_posts: #1932 - Table 'evcolor.wp_posts' doesn't exist in engine
+-- Error reading data for table evcolor.wp_posts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_posts`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_revslider_css`
+--
+-- Error reading structure for table evcolor.wp_revslider_css: #1932 - Table 'evcolor.wp_revslider_css' doesn't exist in engine
+-- Error reading data for table evcolor.wp_revslider_css: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_revslider_css`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_revslider_layer_animations`
+--
+-- Error reading structure for table evcolor.wp_revslider_layer_animations: #1932 - Table 'evcolor.wp_revslider_layer_animations' doesn't exist in engine
+-- Error reading data for table evcolor.wp_revslider_layer_animations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_revslider_layer_animations`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_revslider_navigations`
+--
+-- Error reading structure for table evcolor.wp_revslider_navigations: #1932 - Table 'evcolor.wp_revslider_navigations' doesn't exist in engine
+-- Error reading data for table evcolor.wp_revslider_navigations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_revslider_navigations`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_revslider_sliders`
+--
+-- Error reading structure for table evcolor.wp_revslider_sliders: #1932 - Table 'evcolor.wp_revslider_sliders' doesn't exist in engine
+-- Error reading data for table evcolor.wp_revslider_sliders: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_revslider_sliders`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_revslider_sliders7`
+--
+-- Error reading structure for table evcolor.wp_revslider_sliders7: #1932 - Table 'evcolor.wp_revslider_sliders7' doesn't exist in engine
+-- Error reading data for table evcolor.wp_revslider_sliders7: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_revslider_sliders7`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_revslider_slides`
+--
+-- Error reading structure for table evcolor.wp_revslider_slides: #1932 - Table 'evcolor.wp_revslider_slides' doesn't exist in engine
+-- Error reading data for table evcolor.wp_revslider_slides: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_revslider_slides`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_revslider_slides7`
+--
+-- Error reading structure for table evcolor.wp_revslider_slides7: #1932 - Table 'evcolor.wp_revslider_slides7' doesn't exist in engine
+-- Error reading data for table evcolor.wp_revslider_slides7: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_revslider_slides7`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_revslider_static_slides`
+--
+-- Error reading structure for table evcolor.wp_revslider_static_slides: #1932 - Table 'evcolor.wp_revslider_static_slides' doesn't exist in engine
+-- Error reading data for table evcolor.wp_revslider_static_slides: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_revslider_static_slides`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_termmeta`
+--
+-- Error reading structure for table evcolor.wp_termmeta: #1932 - Table 'evcolor.wp_termmeta' doesn't exist in engine
+-- Error reading data for table evcolor.wp_termmeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_termmeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_terms`
+--
+-- Error reading structure for table evcolor.wp_terms: #1932 - Table 'evcolor.wp_terms' doesn't exist in engine
+-- Error reading data for table evcolor.wp_terms: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_terms`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_term_relationships`
+--
+-- Error reading structure for table evcolor.wp_term_relationships: #1932 - Table 'evcolor.wp_term_relationships' doesn't exist in engine
+-- Error reading data for table evcolor.wp_term_relationships: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_term_relationships`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_term_taxonomy`
+--
+-- Error reading structure for table evcolor.wp_term_taxonomy: #1932 - Table 'evcolor.wp_term_taxonomy' doesn't exist in engine
+-- Error reading data for table evcolor.wp_term_taxonomy: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_term_taxonomy`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_usermeta`
+--
+-- Error reading structure for table evcolor.wp_usermeta: #1932 - Table 'evcolor.wp_usermeta' doesn't exist in engine
+-- Error reading data for table evcolor.wp_usermeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_usermeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_users`
+--
+-- Error reading structure for table evcolor.wp_users: #1932 - Table 'evcolor.wp_users' doesn't exist in engine
+-- Error reading data for table evcolor.wp_users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_users`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_wpforms_logs`
+--
+-- Error reading structure for table evcolor.wp_wpforms_logs: #1932 - Table 'evcolor.wp_wpforms_logs' doesn't exist in engine
+-- Error reading data for table evcolor.wp_wpforms_logs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_wpforms_logs`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_wpforms_payments`
+--
+-- Error reading structure for table evcolor.wp_wpforms_payments: #1932 - Table 'evcolor.wp_wpforms_payments' doesn't exist in engine
+-- Error reading data for table evcolor.wp_wpforms_payments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_wpforms_payments`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_wpforms_payment_meta`
+--
+-- Error reading structure for table evcolor.wp_wpforms_payment_meta: #1932 - Table 'evcolor.wp_wpforms_payment_meta' doesn't exist in engine
+-- Error reading data for table evcolor.wp_wpforms_payment_meta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_wpforms_payment_meta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_wpforms_tasks_meta`
+--
+-- Error reading structure for table evcolor.wp_wpforms_tasks_meta: #1932 - Table 'evcolor.wp_wpforms_tasks_meta' doesn't exist in engine
+-- Error reading data for table evcolor.wp_wpforms_tasks_meta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `evcolor`.`wp_wpforms_tasks_meta`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
